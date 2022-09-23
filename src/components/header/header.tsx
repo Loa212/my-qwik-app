ce2f677c9f487651ac346401a6e4d8f8b94c2a8c
Loa212 <anobileloris@gmail.com>
2022-09-23 19:51:12 +0200

    add drawer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,10 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useContext } from "@builder.io/qwik";
+import { CTX } from "~/routes/layout";
 import { QwikLogo } from "../icons/qwik";
 
 export default component$(() => {
+  const store: { drawerOpen: boolean } = useContext(CTX);
+
   return (
     <header className="flex items-center justify-between max-w-7xl mx-2 xl:mx-auto bg-slate-200/80 px-4 py-2 rounded-md ring ring-slate-700/20 mt-4">
       <div class="logo">
@@ -10,7 +13,7 @@ export default component$(() => {
         </a>
       </div>
       <ul className="flex items-center gap-x-4">
-        <li>
+        <li className="hidden xl:block">
           <a
             href="https://qwik.builder.io/docs/components/overview/"
             target="_blank"
@@ -18,7 +21,7 @@ export default component$(() => {
             Docs
           </a>
         </li>
-        <li>
+        <li className="hidden xl:block">
           <a
             href="https://qwik.builder.io/examples/introduction/hello-world/"
             target="_blank"
@@ -26,7 +29,7 @@ export default component$(() => {
             Examples
           </a>
         </li>
-        <li>
+        <li className="hidden xl:block">
           <a
             href="https://qwik.builder.io/tutorial/welcome/overview/"
             target="_blank"
@@ -34,7 +37,28 @@ export default component$(() => {
             Tutorials
           </a>
         </li>
+        <li>
+          <button
+            className="p-2 hover:ring ring-slate-700 rounded-md"
+            onClick$={() => (store.drawerOpen = true)}
+          >
+            <MenuIcon />
+          </button>
+        </li>
       </ul>
     </header>
   );
 });
+
+export const MenuIcon = ({ size = 28, color = "currentColor", ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M3 5.25v1.5h18v-1.5H3Zm0 6v1.5h18v-1.5H3Zm0 6v1.5h18v-1.5H3Z" />
+  </svg>
+);
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,12 +1,64 @@
-import { component$, Slot } from "@builder.io/qwik";
+import {
+  component$,
+  createContext,
+  Slot,
+  useContextProvider,
+  useStore,
+} from "@builder.io/qwik";
 import Header from "../components/header/header";
 
+export interface BaseLayoutState {
+  drawerOpen: boolean;
+}
+export const CTX = createContext<BaseLayoutState>("baseLayout");
+
 export default component$(() => {
+  const store = useStore({ drawerOpen: false });
+
+  useContextProvider(CTX, store);
+
   return (
     <div className="absolute inset-0 bg-gradient-to-br from-red-600 to-pink-600">
       <main className="">
         <Header />
         <section className="max-w-7xl mx-auto">
+          <div
+            className={
+              " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+              (store.drawerOpen
+                ? " transition-opacity opacity-100 duration-500 translate-x-0  "
+                : " transition-all delay-500 opacity-0 translate-x-full  ")
+            }
+          >
+            <section
+              className={
+                " w-[80vw] max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
+                (store.drawerOpen ? " translate-x-0 " : " translate-x-full ")
+              }
+            >
+              <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+                <button
+                  className="flex items-center gap-x-1 absolute top-3 right-2 px-6 py-2"
+                  onClick$={() => {
+                    store.drawerOpen = false;
+                  }}
+                >
+                  <CloseIcon /> close
+                </button>
+                <header className="p-4 font-bold text-lg">Header</header>
+                <ul>
+                  <li>miao</li>
+                  <li>bau</li>
+                </ul>
+              </article>
+            </section>
+            <section
+              className=" w-screen h-full cursor-pointer "
+              onClick$={() => {
+                store.drawerOpen = false;
+              }}
+            ></section>
+          </div>
           <Slot />
         </section>
       </main>
@@ -18,3 +70,16 @@ export default component$(() => {
     </div>
   );
 });
+
+export const CloseIcon = ({ size = 24, color = "currentColor", ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="m13.41 11.996 4.3-4.29a1.004 1.004 0 1 0-1.42-1.42l-4.29 4.3-4.29-4.3a1.004 1.004 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a.999.999 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1.001 1.001 0 0 0 1.639-.325 1 1 0 0 0-.22-1.095l-4.3-4.29Z" />
+  </svg>
+);
